9e00ea5a80951a1a51aebd5cd7d4e9771300e84a
musbahu ghassan <musbahughassan164@gmail.com>
2023-05-28 16:19:09 +0100

    ranaming the file name from RPS -> Rock_Paper_Scissors.

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,10 +1,9 @@
-import random
+import random # we need to use random module to allow computer to make choice
 
 option = ["r", "p", "s"]
 user_wins = 0
 ai_wins = 0
 
-# rand_no = random.randint(0, 2)
 print("Type 'y' to play or 'q' to quit. ")
 
 while True:
@@ -19,6 +18,7 @@ while True:
     if user_input == ai_pick:
         user_wins += 1
         print("You Win")
+        
     elif user_input != ai_pick:
         ai_wins += 1
         print("A.I Win")
@@ -26,6 +26,7 @@ print("-------You scored: ", user_wins)
 print("-------A.I scored: ", ai_wins)
 if ai_wins > user_wins:
     print("--------You are Eliminated.-------")
+    print("------------TryAgain--------------")
 elif user_wins > ai_wins:
     print("--------You Won The Match.-------")
     print("--------You are The Champion-----")
